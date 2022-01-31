4e2cef4b44e9d3cdf696b2397e1d7d5b5f3f1f74
Ha Chi HOANG <chibienayme@gmail.com>
2022-01-31 11:50:26 +0100

    add form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// CSS
+import "./App.css"
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+			email: "",
+      password: "",
+      isSubmitted: false,
+    }
+    
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      email: e.target.email,
+      password: e.target.password,
+    })
+  }
+
+  handleSubmit(e) {
+		console.log('Your email is' + this.state.email + "Your password is" + this.state.password + 'are submitted');
+    e.preventDefault();
+	}
+
+
+  render() {
+    return(
+      <form onSubmit={this.handleSubmit}>
+        <label> Email: </label>
+        <input type="text" class="form-control p-2 m-2" value={this.state.email} onChange={this.handleChange} />
+
+        <label> Password: </label>
+        <input type="password" class="form-control p-2 m-2" value={this.state.password} onChange={this.handleChange} />
+
+        <input type="checkbox" class="p-2 m-2" value="remember-me" id="remember_me"/>  Remember me
+
+        <input type="submit" class="p-2 m-2" value="Submit"
+        onChange={this.handleSubmit}/>
+
+      </form>
+
+               
+    ) 
+  }
 }
-
-export default App;
+export default App
