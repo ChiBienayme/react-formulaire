f336949c4bdbe5b0d21b6bd7c124a2ac0e8ce85d
Ha Chi HOANG <chibienayme@gmail.com>
2022-01-31 12:30:38 +0100

    add valid and invalid

--- src/App.js
+++ src/App.js
@@ -1,54 +1,82 @@
-import React from "react"
+import React from "react";
 
 // CSS
-import "./App.css"
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 class App extends React.Component {
   constructor() {
     super();
 
     this.state = {
-			email: "",
+      email: "",
       password: "",
       isSubmitted: false,
-    }
-    
-    this.handleChange = this.handleChange.bind(this);
+    };
+
+    this.handleEmail = this.handleEmail.bind(this);
+    this.handlePassword = this.handlePassword.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleChange(e) {
+  handleEmail(e) {
     this.setState({
-      email: e.target.email,
-      password: e.target.password,
-    })
+      email: e.target.value,
+      isSubmitted: true,
+    });
+  }
+  handlePassword(e) {
+    this.setState({
+      password: e.target.value,
+      isSubmitted: true,
+    });
   }
 
   handleSubmit(e) {
-		console.log('Your email is' + this.state.email + "Your password is" + this.state.password + 'are submitted');
+    console.log(
+      "Your email is" +
+        this.state.email +
+        "Your password is" +
+        this.state.password +
+        "are submitted"
+    );
     e.preventDefault();
-	}
-
+  }
 
   render() {
-    return(
-      <form onSubmit={this.handleSubmit}>
+    return (
+      <form className="was-validated" onSubmit={this.handleSubmit}>
         <label> Email: </label>
-        <input type="text" class="form-control p-2 m-2" value={this.state.email} onChange={this.handleChange} />
+        <input
+          type="text"
+          className="form-control is-invalid p-2 m-2 w-50"
+          value={this.state.email}
+          onChange={this.handleEmail}
+        />
 
         <label> Password: </label>
-        <input type="password" class="form-control p-2 m-2" value={this.state.password} onChange={this.handleChange} />
+        <input
+          type="password"
+          className="form-control is-invalid p-2 m-2 w-50"
+          value={this.state.password}
+          onChange={this.handlePassword}
+        />
 
-        <input type="checkbox" class="p-2 m-2" value="remember-me" id="remember_me"/>  Remember me
-
-        <input type="submit" class="p-2 m-2" value="Submit"
-        onChange={this.handleSubmit}/>
-
+        <input
+          type="checkbox"
+          className="p-2 m-2"
+          value="remember-me"
+          id="remember_me"
+        />
+        Remember me
+        <input
+          type="submit"
+          className="p-2 m-2"
+          value="Submit"
+          onChange={this.handleSubmit}
+        />
       </form>
-
-               
-    ) 
+    );
   }
 }
-export default App
+export default App;
